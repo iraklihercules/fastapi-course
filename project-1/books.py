eb6fd11139090eec56520eab528062cccd9c69a2
Irakli <irakli.alaverdashvili@hotmail.com>
2025-11-24 21:34:53 +0400

    Path parameters

--- project-1/books.py
+++ project-1/books.py
@@ -17,6 +17,19 @@ async def first_endpoint():
     return {"message": "Hello world!"}
 
 
+# Path parameters
+@app.get("/params/{dynamic_param}")
+async def dynamic_param(dynamic_param: str):
+    return {"dynamic_param": dynamic_param}
+
+
 @app.get("/books")
 async def get_books():
     return BOOKS
+
+
+@app.get("/books_by_title/{book_title}")
+async def get_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book

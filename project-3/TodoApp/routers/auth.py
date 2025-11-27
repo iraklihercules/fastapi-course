752bfd54ccc58f520aa6b68116f9f62fb0451371
Irakli <irakli.alaverdashvili@hotmail.com>
2025-11-28 00:41:52 +0400

    Save user to database

--- project-3/TodoApp/routers/auth.py
+++ project-3/TodoApp/routers/auth.py
@@ -1,8 +1,26 @@
-from fastapi import APIRouter
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
+from database import SessionLocal
+from starlette import status
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 
 class UserRequest(BaseModel):
@@ -15,20 +33,17 @@ class UserRequest(BaseModel):
     role: str
 
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
-
-
-async def create_usel(user_request: UserRequest):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, user_request: UserRequest):
+    hashed_password = bcrypt_context.hash(user_request.password)
     user_model = Users(
         email=user_request.email,
         username=user_request.username,
         first_name=user_request.first_name,
         last_name=user_request.last_name,
-        hashed_password=user_request.password,
+        hashed_password=hashed_password,
         is_active=True,
         role=user_request.role,
     )
-
-    return user_model
+    db.add(user_model)
+    db.commit()

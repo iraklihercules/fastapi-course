9313054da162e633d45741ffb9e7f7cc155fe012
Irakli <irakli.alaverdashvili@hotmail.com>
2025-11-27 23:53:54 +0400

    Authenticate a user

--- project-3/TodoApp/routers/auth.py
+++ project-3/TodoApp/routers/auth.py
@@ -6,6 +6,7 @@ from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -33,6 +34,13 @@ class UserRequest(BaseModel):
     role: str
 
 
+def authenticate_user(username: str, password: str, db: db_dependency) -> bool:
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    return bcrypt_context.verify(password, user.hashed_password)
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, user_request: UserRequest):
     hashed_password = bcrypt_context.hash(user_request.password)
@@ -47,3 +55,13 @@ async def create_user(db: db_dependency, user_request: UserRequest):
     )
     db.add(user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed"
+    return "Success"
